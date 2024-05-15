22b645b6cab3a77d64b8df782576f940ae0c70f6
kraksasha <kraksasha@yandex.ru>
2024-05-15 19:44:57 +0300

    comm

--- src/main/java/org/example/Trainee.java
+++ src/main/java/org/example/Trainee.java
@@ -6,7 +6,7 @@ public class Trainee {
     public static void main(String[] args) {
         long massive[] = {10,5,-3,6,8,9,2,5};
         System.out.println(Arrays.toString(solution(massive)));
-        solutionSort(new int[]{32,4,6,7},new int[]{32,4,6,7},new int[]{32,5,6,2,7,4,6});
+        System.out.println(Arrays.toString(solutionSort(new int[]{12,5,1,7},new int[]{32,4,6,7},new int[]{32,5,6,2,7,4,6})));
     }
 
     public static long[] solution(long array[]){
@@ -23,12 +23,21 @@ public class Trainee {
         return result;
     }
 
-    public static int[] solutionSort(int a[], int b[], int c[]){
-        ArrayList<Integer> list = new ArrayList<>();
+    public static int[] solutionSort(int a[], int b[], int c[]) {
         int res[] = new int[a.length + b.length + c.length];
-        System.arraycopy(a,0,res,0,a.length);
-        System.arraycopy(b,0,res,a.length,b.length);
-        System.arraycopy(c,0,res,b.length,c.length);
-        // не успеваю тут можно метод сортировки пузырьком применить
+        System.arraycopy(a, 0, res, 0, a.length);
+        System.arraycopy(b, 0, res, a.length, b.length);
+        System.arraycopy(c, 0, res, a.length +  b.length, c.length);
+        for (int i = 0; i < res.length; i++) {
+            int n = 0;
+            for (int j = i + 1; j < res.length; j++) {
+                if (res[i] > res[j]) {
+                    n = res[i];
+                    res[i] = res[j];
+                    res[j] = n;
+                }
+            }
+        }
+        return res;
     }
 }
